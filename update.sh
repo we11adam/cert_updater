b2f83c18049543b399cb6cbe51a6e592c7a49c89
Mac_Zhou <mac_zhou@live.com>
2019-01-30 14:04:53 +0800

    feat: support custom ssh port and username

--- update.sh
+++ update.sh
@@ -3,7 +3,7 @@
 # @author: http://github.com/welladamm/
 
 
-CERT_NAME="midway.io"
+CERT_NAME="you_domain.com"
 
 LOCAL_DIR=$(cd $(dirname $0); pwd -P)
 LOCAL_CERT_DIR="$LE_WORKING_DIR/$CERT_NAME"
@@ -13,55 +13,64 @@ SCP="scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -q"
 
 update_cert_for_dsm() {
     local HOST="$1"
+    local PORT=${2:-22}
+    local USER=${3:-root}
     local REMOTE_SYS_DEFAULT_CERT_DIR="/usr/syno/etc/certificate/system/default"
-    echo "Ready to update HTTPS certificate on Synology system: $HOST"
+    echo -e "[$(date)] \033[32m== Ready to update HTTPS certificate on Synology system: $HOST ==\033[0m"
     local REMOTE_CERT_BASE_DIR="/usr/syno/etc/certificate/_archive"
-    local REMOTE_DEFAULT_CERT_NAME=$($SSH root@$HOST "cat $REMOTE_CERT_BASE_DIR/DEFAULT")
+    local REMOTE_DEFAULT_CERT_NAME=$($SSH -p $PORT $USER@$HOST "cat $REMOTE_CERT_BASE_DIR/DEFAULT")
     local REMOTE_DEFAULT_CERT_DIR="$REMOTE_CERT_BASE_DIR/$REMOTE_DEFAULT_CERT_NAME"
-    echo "Remote default cert dir is $REMOTE_DEFAULT_CERT_DIR"
-    echo "Copying new certificate to $HOST..."
+    echo -e "[$(date)] \033[32m Remote default cert dir is $REMOTE_DEFAULT_CERT_DIR \033[0m"
+    echo -e "[$(date)] \033[32m Copying new certificate to $HOST... \033[0m"
 
-    $SCP $LOCAL_CERT_DIR/fullchain.cer root@$HOST:$REMOTE_DEFAULT_CERT_DIR/fullchain.pem >/dev/null 2>&1 && \
-        $SCP $LOCAL_CERT_DIR/fullchain.cer root@$HOST:$REMOTE_SYS_DEFAULT_CERT_DIR/fullchain.pem >/dev/null 2>&1 && \
-        echo "Copied fullchain.cer"
+    $SCP -P $PORT $LOCAL_CERT_DIR/fullchain.cer $USER@$HOST:$REMOTE_DEFAULT_CERT_DIR/fullchain.pem >/dev/null 2>&1 && \
+        $SCP $LOCAL_CERT_DIR/fullchain.cer $USER@$HOST:$REMOTE_SYS_DEFAULT_CERT_DIR/fullchain.pem >/dev/null 2>&1 && \
+        echo -e "[$(date)] \033[32m Copied fullchain.cer \033[0m"
 
-    $SCP $LOCAL_CERT_DIR/$CERT_NAME.key root@$HOST:$REMOTE_DEFAULT_CERT_DIR/privkey.pem  >/dev/null 2>&1 && \
-        $SCP $LOCAL_CERT_DIR/$CERT_NAME.key root@$HOST:$REMOTE_SYS_DEFAULT_CERT_DIR/privkey.pem >/dev/null 2>&1 && \
-        echo "Copied $CERT_NAME.key"
+    $SCP -P $PORT $LOCAL_CERT_DIR/$CERT_NAME.key $USER@$HOST:$REMOTE_DEFAULT_CERT_DIR/privkey.pem  >/dev/null 2>&1 && \
+        $SCP $LOCAL_CERT_DIR/$CERT_NAME.key $USER@$HOST:$REMOTE_SYS_DEFAULT_CERT_DIR/privkey.pem >/dev/null 2>&1 && \
+        echo -e "[$(date)] \033[32m Copied $CERT_NAME.key \033[0m"
 
-    $SCP $LOCAL_CERT_DIR/$CERT_NAME.cer root@$HOST:$REMOTE_DEFAULT_CERT_DIR/cert.pem  >/dev/null 2>&1 && \
-        $SCP $LOCAL_CERT_DIR/$CERT_NAME.cer root@$HOST:$REMOTE_SYS_DEFAULT_CERT_DIR/cert.pem  >/dev/null 2>&1 && \
-        echo "Copied $CERT_NAME.cer"
+    $SCP -P $PORT $LOCAL_CERT_DIR/$CERT_NAME.cer $USER@$HOST:$REMOTE_DEFAULT_CERT_DIR/cert.pem  >/dev/null 2>&1 && \
+        $SCP $LOCAL_CERT_DIR/$CERT_NAME.cer $USER@$HOST:$REMOTE_SYS_DEFAULT_CERT_DIR/cert.pem  >/dev/null 2>&1 && \
+        echo -e "[$(date)] \033[32m Copied $CERT_NAME.cer \033[0m"
 
-    echo "Done!"
-    $SSH root@$HOST "/usr/syno/sbin/synoservicectl --restart nginx"
-    echo "HTTPS certificate on $HOST is now renewed & updated!"
+    echo -e "[$(date)] \033[32m Done! \033[0m"
+    $SSH -p $PORT $USER@$HOST "/usr/syno/sbin/synoservicectl --restart nginx"
+    echo -e "[$(date)] \033[32m== HTTPS certificate on $HOST is now renewed & updated! ==\033[0m"
 }
 
 update_cert_for_esxi() {
     local HOST="$1"
-    echo "Ready to update HTTPS certificate on ESXi system: $HOST"
-    $SCP $LOCAL_CERT_DIR/$CERT_NAME.key root@$HOST:/etc/vmware/ssl/rui.key
-    $SCP $LOCAL_CERT_DIR/$CERT_NAME.cer root@$HOST:/etc/vmware/ssl/rui.crt
-    $SSH root@$HOST "/etc/init.d/hostd restart"
-    echo "HTTPS certificate on $HOST is now renewed & updated!"
+    local PORT=${2:-22}
+    local USER=${3:-root}
+    echo -e "[$(date)] \033[32m== Ready to update HTTPS certificate on ESXi system: $HOST ==\033[0m"
+    $SCP -P $PORT $LOCAL_CERT_DIR/$CERT_NAME.key $USER@$HOST:/etc/vmware/ssl/rui.key
+    $SCP -P $PORT $LOCAL_CERT_DIR/$CERT_NAME.cer $USER@$HOST:/etc/vmware/ssl/rui.crt
+    $SSH -p $PORT $USER@$HOST "/etc/init.d/hostd restart"
+    echo -e "[$(date)] \033[32m== HTTPS certificate on $HOST is now renewed & updated! ==\033[0m"
 }
 
 update_cert_for_unifi() {
     local HOST="$1"
-    $SCP $LOCAL_CERT_DIR/$CERT_NAME.key root@$HOST:/tmp/$CERT_NAME.key
-    $SCP $LOCAL_CERT_DIR/$CERT_NAME.cer root@$HOST:/tmp/$CERT_NAME.cer
-    $SCP $LOCAL_CERT_DIR/fullchain.cer root@$HOST:/tmp/$CERT_NAME.chain
-    $SSH root@$HOST "openssl pkcs12 -export -inkey /tmp/$CERT_NAME.key -in /tmp/$CERT_NAME.cer -certfile /tmp/$CERT_NAME.chain -out /tmp/$CERT_NAME.p12 -name unifi -password pass:temppass"
-    $SSH root@$HOST "keytool -importkeystore -deststorepass aircontrolenterprise -destkeypass aircontrolenterprise -destkeystore /var/lib/unifi/keystore -srckeystore /tmp/$CERT_NAME.p12 -srcstoretype PKCS12 -srcstorepass temppass -alias unifi -noprompt"
-    $SSH root@$HOST "service unifi restart"
-    $SSH root@$HOST "rm /tmp/$CERT_NAME.*"
+    local PORT=${2:-22}
+    local USER=${3:-root}
+    echo -e "[$(date)] \033[32m== Ready to update HTTPS certificate on Unifi Controller system: $HOST ==\033[0m"
+    $SCP -P $PORT $LOCAL_CERT_DIR/$CERT_NAME.key $USER@$HOST:/tmp/$CERT_NAME.key
+    $SCP -P $PORT $LOCAL_CERT_DIR/$CERT_NAME.cer $USER@$HOST:/tmp/$CERT_NAME.cer
+    $SCP -P $PORT $LOCAL_CERT_DIR/fullchain.cer $USER@$HOST:/tmp/$CERT_NAME.chain
+    $SSH -p $PORT $USER@$HOST "openssl pkcs12 -export -inkey /tmp/$CERT_NAME.key -in /tmp/$CERT_NAME.cer -certfile /tmp/$CERT_NAME.chain -out /tmp/$CERT_NAME.p12 -name unifi -password pass:temppass"
+    $SSH -p $PORT $USER@$HOST "keytool -importkeystore -deststorepass aircontrolenterprise -destkeypass aircontrolenterprise -destkeystore /var/lib/unifi/keystore -srckeystore /tmp/$CERT_NAME.p12 -srcstoretype PKCS12 -srcstorepass temppass -alias unifi -noprompt >/dev/null 2>&1"
+    $SSH -p $PORT $USER@$HOST "service unifi restart"
+    $SSH -p $PORT $USER@$HOST "rm /tmp/$CERT_NAME.*"
+    echo -e "[$(date)] \033[32m== HTTPS certificate on $HOST is now renewed & updated! ==\033[0m"
 }
 
 update_cert_for_routeros() {
     local HOST="$1"
-    local PORT="$2"
-    local USER="$3"
+    local PORT=${2:-22}
+    local USER=${3:-root}
+    echo -e "[$(date)] \033[32m== Ready to update HTTPS certificate on RouterOS system: $HOST ==\033[0m"
     $SCP -P $PORT $LOCAL_CERT_DIR/$CERT_NAME.key $USER@$HOST:$CERT_NAME.key
     $SCP -P $PORT $LOCAL_CERT_DIR/$CERT_NAME.cer $USER@$HOST:$CERT_NAME.cer
     $SCP -P $PORT $LOCAL_CERT_DIR/ca.cer $USER@$HOST:letsencrypt-ca.cer
@@ -74,26 +83,34 @@ update_cert_for_routeros() {
     $SSH -p $PORT $USER@$HOST /file remove $CERT_NAME.cer
     $SSH -p $PORT $USER@$HOST /file remove letsencrypt-ca.cer
     $SSH -p $PORT $USER@$HOST /ip service set www-ssl certificate=$CERT_NAME.cer_0
+    echo -e "[$(date)] \033[32m== HTTPS certificate on $HOST is now renewed & updated! ==\033[0m"
 }
 
 update_cert_for_plex() {
     local HOST="$1"
-    $SCP $LOCAL_CERT_DIR/$CERT_NAME.key root@$HOST:/tmp/$CERT_NAME.key
-    $SCP $LOCAL_CERT_DIR/$CERT_NAME.cer root@$HOST:/tmp/$CERT_NAME.cer
-    $SCP $LOCAL_CERT_DIR/fullchain.cer root@$HOST:/tmp/$CERT_NAME.chain
-    $SSH root@$HOST "openssl pkcs12 -export -inkey /tmp/$CERT_NAME.key -in /tmp/$CERT_NAME.cer -certfile /tmp/$CERT_NAME.chain -out /tmp/$CERT_NAME.p12 -name plex -password pass:temppass"
-    $SSH root@$HOST '"/var/packages/Plex Media Server/scripts/start-stop-status" restart'
+    local PORT=${2:-22}
+    local USER=${3:-root}
+    echo -e "[$(date)] \033[32m== Ready to update HTTPS certificate on Plex Server system: $HOST ==\033[0m"
+    $SCP -P $PORT $LOCAL_CERT_DIR/$CERT_NAME.key $USER@$HOST:/tmp/$CERT_NAME.key
+    $SCP -P $PORT $LOCAL_CERT_DIR/$CERT_NAME.cer $USER@$HOST:/tmp/$CERT_NAME.cer
+    $SCP -P $PORT $LOCAL_CERT_DIR/fullchain.cer $USER@$HOST:/tmp/$CERT_NAME.chain
+    $SSH -p $PORT $USER@$HOST "openssl pkcs12 -export -inkey /tmp/$CERT_NAME.key -in /tmp/$CERT_NAME.cer -certfile /tmp/$CERT_NAME.chain -out /tmp/$CERT_NAME.p12 -name plex -password pass:temppass"
+    $SSH -p $PORT $USER@$HOST '"/var/packages/Plex Media Server/scripts/start-stop-status" restart'
+    echo -e "[$(date)] \033[32m== HTTPS certificate on $HOST is now renewed & updated! ==\033[0m"
 }
 
 
-update_cert_for_dsm "your_dsm_host"
+#The default SSH port is 22
+#The default SSH username is root.
+#You can set custom SSH port or username,
+#Example: update_cert_for_routeros 10.0.0.11 1022 admin
+
+update_cert_for_dsm "your_dsm_host" 
 
 update_cert_for_esxi "your_esxi_host"
 
 update_cert_for_unifi "your_unifi_controller_host"
 
-update_cert_for_routeros "your_routeros_host" "router_os_ssh_port" "username"
+update_cert_for_routeros "your_routeros_host"
 
 update_cert_for_plex "your_plex_host"
-
-
